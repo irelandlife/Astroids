12c2297045abbc5ab67cbab56df17ee6f95f011f
Robert Ireland <rgireland@ucdavis.edu>
2020-09-28 13:04:26 -0700

    Add files via upload

---
+++ build/precache-manifest.37f0846a800de0b6c63053ddb4bdd094.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843e43fa4f4fc40801049a94162f409f",
+    "url": "/Astroids/index.html"
+  },
+  {
+    "revision": "9acdfb83918151ec433a",
+    "url": "/Astroids/static/js/2.9984796e.chunk.js"
+  },
+  {
+    "revision": "f5a5ff1f96889814dd83b6008159ed69",
+    "url": "/Astroids/static/js/2.9984796e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd760e7f8da9ec507425",
+    "url": "/Astroids/static/js/main.980129bf.chunk.js"
+  },
+  {
+    "revision": "e2b9fa1e57e8f3bc7c9a",
+    "url": "/Astroids/static/js/runtime-main.8e637e99.js"
+  },
+  {
+    "revision": "a93d8e1a89f9100d7e04c98ebcbc0933",
+    "url": "/Astroids/static/media/bg.a93d8e1a.mp3"
+  },
+  {
+    "revision": "8d516cce9b80ee257214da40ace5c474",
+    "url": "/Astroids/static/media/click.8d516cce.mp3"
+  },
+  {
+    "revision": "12d1603454265a90269ca13e0d6c04b2",
+    "url": "/Astroids/static/media/earth.12d16034.jpg"
+  },
+  {
+    "revision": "e896d081c42d144db443a0faeea36cf8",
+    "url": "/Astroids/static/media/engine.e896d081.mp3"
+  },
+  {
+    "revision": "bdb080913490e33da93bb6510d530d4d",
+    "url": "/Astroids/static/media/engine2.bdb08091.mp3"
+  },
+  {
+    "revision": "5c5ce6974ee4192d9ecbc74242daa4ce",
+    "url": "/Astroids/static/media/explosion.5c5ce697.mp3"
+  },
+  {
+    "revision": "a97c8e670e85efbd49691d7e343da830",
+    "url": "/Astroids/static/media/laser.a97c8e67.mp3"
+  },
+  {
+    "revision": "8e294414ed8fefd6cc5fe6480adeaa7b",
+    "url": "/Astroids/static/media/moon.8e294414.png"
+  },
+  {
+    "revision": "5699f1c4aff5845551ef0ba1a7ad31f8",
+    "url": "/Astroids/static/media/warp.5699f1c4.mp3"
+  }
+]);
